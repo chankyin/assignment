1efc6142fe22b92e56612ace7d7dde178bdfe228
chankyin <u3546697@hku.hk>
2017-10-11 20:58:07 +0800

    Missing cd

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for dir in ./a*/; do
-    g++ -pedantic-errors -std=c++11 ${dir}/test.cpp -o ${dir}/test && (echo 0 | a1/test)
+    g++ -pedantic-errors -std=c++11 ${dir}/test.cpp -o ${dir}/test && (cd ${dir} ; echo 0 | ${dir}/test)
 done
