289afdcf56ed54b1840d8b65a66467cded2dc823
chankyin <u3546697@hku.hk>
2017-10-25 21:31:03 +0800

    Assignment 2 (2)

--- a2/2.cpp
+++ a2/2.cpp
@@ -4,6 +4,42 @@
 
 //   Write your function definitions here
 
+void read_word(char *word, int &arrayLength){
+  std::cin >> arrayLength;
+  for(int i = 0; i < arrayLength; ++i){
+    std::cin >> word[i];
+  }
+}
+
+char read_target(){
+  char c;
+  std::cin >> c;
+  return c;
+}
+
+void delete_target(char *word, int &wordLength, const char &target){
+  int matchCount = 0;
+  bool matches[10] = {};
+  for(int i = 0; i < wordLength; ++i){
+    if(word[i] == target){
+      matches[i] = true;
+      ++matchCount;
+    }
+  }
+  wordLength -= matchCount;
+  int shiftSize = 0;
+  for(int i = 0; i < wordLength; ++i){
+    if(matches[i]){
+      ++shiftSize;
+    }
+    word[i] = word[i + shiftSize];
+    // assertion: i + shiftSize < o_wordLength (where o_wordLength is the original value of wordLength, i.e. wordLength + matchCount
+    // proof: i and shiftSize both attain maximum in the last loop
+    // i.e. i = wordLength - 1 and shiftSize = matchCount,
+    // assuming matchCount = number of true values in matches.slice(0, o_wordLength)
+  }
+}
+
 //----------------------DO NOT CHANGE ANYTHING BELOW THIS LINE------------------
 
 int main()
