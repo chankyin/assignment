c454bfdb120d8052a7ea8e5334cf53ca3b450bd7
chankyin <u3546697@hku.hk>
2017-10-25 20:12:19 +0800

    Assignment 2 (1)

--- a2/1.cpp
+++ a2/1.cpp
@@ -5,6 +5,36 @@ const int SIZE = 8;
 
 //   Write your function definitions here
 
+void read_array(int *array){
+  for(int i = 0; i < SIZE; ++i){
+    std::cin >> array[i];
+  }
+}
+
+int read_number_of_shifts(){
+  int result;
+  std::cin >> result;
+  return result;
+}
+
+void shift_array(int *array, int shifts){
+  shifts %= SIZE;
+  if(shifts < 0){
+    shifts += SIZE;
+  }
+  int clone[SIZE]{};
+  for(int i = 0; i < SIZE; ++i){
+    int offset = i - shifts;
+    if(offset < 0){
+      offset += SIZE;
+    }
+    clone[i] = array[offset];
+  }
+  for(int i = 0; i < SIZE; ++i){
+    array[i] = clone[i];
+  }
+}
+
 //----------------------DO NOT CHANGE ANYTHING BELOW THIS LINE------------------
 int main()
 {
