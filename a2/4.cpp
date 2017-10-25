75897606f6ba82e88dab17553a77193d83bca8c1
chankyin <u3546697@hku.hk>
2017-10-25 22:47:59 +0800

    Assignment 2 (4)

--- a2/4.cpp
+++ a2/4.cpp
@@ -6,6 +6,26 @@ const int MAXDIM = 10;
 
 //   Write your function definitions here
 
+void read_matrix(int matrix[MAXDIM][MAXDIM], int &dim){
+  std::cin >> dim;
+  for(int y = 0; y < dim; ++y){
+    for(int x = 0; x < dim; ++x){
+      std::cin >> matrix[x][y];
+    }
+  }
+}
+
+void display_inner_cw_rotated(int matrix[MAXDIM][MAXDIM], int dim){
+  for(int y = 1; y < dim - 1; ++y){
+    bool first = true;
+    for(int x = 1; x < dim - 1; ++x){
+      std::cout << matrix[y][dim - 1 - x] << " ";
+      first = false;
+    }
+    std::cout << std::endl;
+  }
+}
+
 //----------------------DO NOT CHANGE ANYTHING BELOW THIS LINE------------------
 
 int main()
